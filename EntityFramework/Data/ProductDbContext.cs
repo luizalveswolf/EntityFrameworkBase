cac1f9e5f181c3f364cb9eeb93e4a150d9a7f6cd
luizalveswolf <luiz.alves.wolf@gmail.com>
2018-03-15 16:49:13 -0300

    vai

---
+++ EntityFramework/Data/ProductDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Domain;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public class ProductDbContext : DbContext
+    {
+        public ProductDbContext() : base("Name=ProductDb")
+        {
+            Database.SetInitializer<ProductDbContext>(
+                new CreateDatabaseIfNotExists<ProductDbContext>());
+            Database.Initialize(false);
+        }
+
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Loja> Lojas { get; set; }
+    }
+}
---
+++ EntityFramework/Domain/Produto.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    [Table("Produto")]
+    public class Produto
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [MaxLength(200)]
+        [Required]
+        public string Nome { get; set; }
+
+        [MaxLength(2000)]
+        public string Descricao { get; set; }
+
+        [Range(-9999999999999.99, 9999999999999.99)]
+        [Required]
+        public decimal Valor { get; set; }
+
+        [ForeignKey("Loja")]
+        public int LojaId { get; set; }
+        
+        public virtual Loja Loja { get; set; }
+    }
+}
